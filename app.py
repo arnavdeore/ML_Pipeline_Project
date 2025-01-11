b96d5e47b067bb02fbe9a088d248bbf4c82055f1
arnavdeore <arnavdeore08@gmail.com>
2025-01-11 14:31:47 +0530

    Flask Application created

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-from flask import Flask
-from src.logger import logging
+from flask import Flask, render_template, request, jsonify
+from src.pipeline.prediction_pipeline import PredictionPipelin, CustomClass
 
 app = Flask(__name__)
+@app.route("/",methods = ["GET", "POST"])
 
-@app.route('/', methods = ['GET','POST'])
+def prediction_data():
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        data = CustomClass(
+         age = int(request.form.get("age")),
+         workclass = int(request.form.get("workclass")),
+         education_num = int(request.form.get("education_num")),
+         marital_status = int(request.form.get("marital_status")),
+         occupation = int(request.form.get("occupation")),
+         relationship = int(request.form.get("relationship")),
+         race = int(request.form.get("race")),
+         sex = int(request.form.get("sex")),
+         capital_gain = int(request.form.get("capital_gain")),
+         capital_loss = int(request.form.get("capital_loss")),
+         hours_per_week = int(request.form.get("hours_per_week")),
+         native_country = int(request.form.get("native_country")),   
+        )
+    
+    final_data = data.get_data_DataFrame()
+    pipeline_prediction = PredictionPipelin()
+    pred = pipeline_prediction.predict(final_data)
 
-def index():
-    logging.info("testing second methond of logging")
-    return "Welcome to the ML pipeline Project"
+    result = pred
 
-if __name__ =="__main__":
-    app.run(debug=True)
+    if result == 0:
+        return render_template("result.html", final_result = "Your yearly income is less than equal to $50k:{}".format(result))
+    elif result == 1:
+        return render_template("resul.html", final_result = "Your yearly income is more than $50k:{}".format(result))
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
